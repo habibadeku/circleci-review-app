81cc72614ef87cd4a1dcd4dd3233ed585efdc6ae
habib <40292697+habibadeku@users.noreply.github.com>
2021-01-23 11:18:36 +0100

    first commit with circle ci config

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,13 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browsers: ['Chrome'], // added config
+    customLaunchers: { // added config
+        ChromeHeadlessCI: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+        }
+    }
   });
 };
